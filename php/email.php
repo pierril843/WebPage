04d78fa8c0abee2bb821612bb84e2a4d40236dfe
Robert R. Pierce <pierril843@gmail.com>
2019-08-04 22:36:16 -0400

    stash start

---
+++ php/email.php
@@ -0,0 +1,14 @@
+<?php
+
+if($_POST["submit"]) {
+    $recipient="pierril843@gmail.com";
+    $subject="Form to email message";
+    $sender=$_POST["name"];
+    $senderEmail=$_POST["email"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+}
+?>
